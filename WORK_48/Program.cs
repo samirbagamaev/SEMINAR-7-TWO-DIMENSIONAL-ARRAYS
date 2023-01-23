cf81a52a50c25bd7e153dd19a164dd3d63925414
Samir Bagamaev <05ru4315@gmail.com>
2023-01-23 20:56:16 +0300

    WORK_48

---
+++ WORK_48/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+{
+    int [,] matrix = new int[rows, columns];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+const int ROWS = 3; // 3 СТРОКИ
+const int COLUMNS = 4; // 4 СТОЛБЦА
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 10;
+
+int[,] array = GetRandomMatrix(ROWS, COLUMNS, LEFTRANGE, RIGHTRANGE);
+ PrintMatrix(array);
