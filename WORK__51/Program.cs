1c799b933676fac73a049e1edeaeb4dbc96d1630
Samir Bagamaev <05ru4315@gmail.com>
2023-01-24 03:01:01 +0300

    WORK_51

---
+++ WORK__51/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+// ​
+// Доп. условие для 51 задачи: сделать суммирование в один цикл.
+// Матрица может быть прямоугольной
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+{
+    int [,] matrix = new int[rows, columns];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+int GetMainDiagonal(int[,] matrix)
+{
+    int result = 0;
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
+    {
+        result += matrix[i, i];
+    }
+    return result;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+  for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+const int ROWS = 3; // 3 СТРОКИ
+const int COLUMNS = 4; // 4 СТОЛБЦА
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 10;
+
+int[,] resultMatrix = GetRandomMatrix(ROWS, COLUMNS, LEFTRANGE, RIGHTRANGE);
+ PrintMatrix(resultMatrix);
+int sum = GetMainDiagonal(resultMatrix);
+Console.WriteLine(sum);
+
